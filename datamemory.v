ec25948d4ee9cebfd5f45ab7711025f20a5b75db
faroukosama2 <1200973@eng.asu.edu.eg>
2015-12-17 23:25:03 +0200

    Update datamemory.v

--- datamemory.v
+++ datamemory.v
@@ -1,52 +1,37 @@
-module data_memory#(parameter size_ward=2)(read_data,address,write_data,clk,memwrite,memread);
+module dataaaaa_memory#(parameter size_ward=2)(read_data,address,write_data,clk,memwrite,memread);
 output reg[31:0]read_data;
 input [31:0]write_data;
 input [$clog2(4*size_ward)-1:0]address;
 input memread,memwrite,clk;
 reg [7:0] regester [0:size_ward*4-1];
-integer i,j;
 //async read
 always@(posedge  clk or address )
-	  #200
-	  for(j=3;j>=0;j=j-1)
-	     begin
-		 for(i=0;i<8;i=i+1)
-		 read_data[j*8+i]<=regester[address+3-j][i];
-	     end
+	#200    read_data<={regester[address],regester[address+1],regester[address+2],regester[address+3]};
 always @(posedge clk)
 	 #500
 	  if(memwrite&!memread)
-	   begin
-	      #200
-		for(j=3;j>=0;j=j-1)
-		 begin
-		  for(i=0;i<8;i=i+1)
-		  regester[address+3-j][i]<=write_data[j*8+i];
-		 end		
-		#1 $writememh("datamem.txt",regester);
-	   end
+	   fork
+	#200	regester[address]<=write_data[31:24];
+	#200	regester[address+1]<=write_data[23:16];
+	#200	regester[address+2]<=write_data[15:8];
+	#200	regester[address+3]<=write_data[7:0];
+	#201    $writememh("datamem.txt",regester);
+	   join
   
 //sync read
 /*always @(posedge clk)
    begin
 	 #500
 	 if(memread&!memwrite)
-	  #200
-	  for(j=3;j>=0;j=j-1)
-	     begin
-		 for(i=0;i<8;i=i+1)
-		 read_data[j*8+i]<=regester[address+3-j][i];
-	     end
+	#200    read_data<={regester[address],regester[address+1],regester[address+2],regester[address+3]};
 	 else if(memwrite&!memread)
-	   begin
-	      #200
-		for(j=3;j>=0;j=j-1)
-		 begin
-		  for(i=0;i<8;i=i+1)
-		  regester[address+3-j][i]<=write_data[j*8+i];
-		 end		
-		#1 $writememh("datamem.txt",regester);
-	   end
+	     fork
+	#200	regester[address]<=write_data[31:24];
+	#200	regester[address+1]<=write_data[23:16];
+	#200	regester[address+2]<=write_data[15:8];
+	#200	regester[address+3]<=write_data[7:0];
+	#201    $writememh("datamem.txt",regester);
+	   join
    end	
 */
 initial
